6200e7ac362af8f754b80e5f7389ebb12a84aada
Peter Fraedrich <iops@hexapp.net>
2018-05-29 19:14:12 -0700

    move to xubuntu

--- main.js
+++ main.js
@@ -1,19 +1,53 @@
 #!/usr/bin/env node
 
 //// IMPORTS ////
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, BrowserView } = require('electron')
 const path = require('path')
 const url = require('url')
-const lowdb = require('lowdb')
-const config = require('./lib/config.js')
+const fs = require('fs')
+
+const Actions = require('./lib/actions.js')
 
 let mainwindow
-
-config.init().then(console.log('db created'))
-config.get().then(
-    function(ok) { console.log(ok)}
-)
+let config
 
 function createMainWindow() {
-    mainwindow = new BrowserWindow()
+    defaults = {
+        window : {
+            width : 800,
+            height: 600,
+            show: false
+        },
+        preferences : {
+            remember_window_position : true,
+            open_dev_tools_at_startup : false,
+            reopen_tabs_at_startup : false
+        },
+        application : {
+            logging: false
+        }
+    }
+    if (! fs.existsSync('db.json')) {
+        fs.writeFileSync('db.json', JSON.stringify(defaults))
+        config = defaults
+    } else {
+        config = JSON.parse(fs.readFileSync('db.json'))
+    }
+
+    mainwindow = new BrowserWindow(config.window)
+
+    // load new tab
+    Actions.newtab().then((ok, err) => {
+            mainwindow.loadURL(ok)
+    })
+
+    if (config.preferences.open_dev_tools_at_startup == true) {
+        mainwindow.webContents.openDevTools()
+    }
+
+    mainwindow.once('ready-to-show', () => {
+        //mainwindow.show()
+    })
 }
+
+app.on('ready', createMainWindow)
